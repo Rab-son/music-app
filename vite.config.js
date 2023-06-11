01b1ef278c9841b734a0be175e799ca77adf61cd
Rab-son <rabsonsayendajnr@gmail.com>
2023-06-11 16:43:32 +0200

    feat: completed app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { sveltekit } from '@sveltejs/kit/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+};
+
+export default config;
